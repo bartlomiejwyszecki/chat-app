db118eb1035895aa180a5a769f4811607782bdd3
unknown <bartlomiej.wyszecki2@gmail.com>
2021-06-15 20:35:37 +0200

    Provided location to user actually sending message.

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,20 +3,31 @@ const socket = io()
 socket.on('message', (message) => {
     console.log(message)
 })
-// socket.on('countUpdated', (count) => {
-//     console.log('The count has been updated', count)
-// })
-
-// document.querySelector('#increment').addEventListener('click', () => {
-//     console.log('Clicked')
-//     socket.emit('increment')
-// })
 
 document.querySelector('#messageForm').addEventListener('submit', (event) => {
     event.preventDefault()
     
-    let text = document.getElementById('msg')
+    let text = event.target.elements.msg
 
-    socket.emit('sendMessage', text.value)
+    socket.emit('sendMessage', text.value, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
     text.value = ''
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,9 +19,25 @@ io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined.')
 
-    socket.on('sendMessage', (msg) => {
+    socket.on('sendMessage', (msg, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(msg)) {
+            return callback('Profanity is not allowed!')
+        }
+        
         io.emit('message', msg)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     })
 })
 
